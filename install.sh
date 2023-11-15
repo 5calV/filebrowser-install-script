c0dc3e811c5bdf45ab06717967d988ce085b5701
5calV <110572069+5calV@users.noreply.github.com>
2023-11-15 14:32:08 +0100

    created empty config file

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-apt update && apt full -upgrade -y
+echo "This script will perform an automatic installation of FileBrowser!"
+sleep 1
+echo "Updating repositories..."
+sleep 0.5
 
+apt update && apt full -upgrade -y && apt install curl -y
+sleep 1
+
+echo "Updates done."
+sleep 0.5
+echo "Downloading and installing FileBrowser..."
+sleep 0.5
+
+curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
+
+sleep 0.5
+echo "Creating config file in /etc/filebrowser.json ..."
+sleep 0.5
+
+touch /etc/filebrowser.json
+
+echo
